c91165c43e8314ee52d9044953f9274ebdb70940
Sergio <sergioruza22@gmail.com>
2023-05-17 16:38:48 -0300

    feat: search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,12 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 def search_by_title(title):
     title_insensitive = {"$regex": title, "$options": "i"}
     news = search_news({"title": title_insensitive})
+    print(news)
     list_news = []
-
     for post in news:
         list_news.append((post["title"], post["url"]))
 
@@ -13,7 +14,24 @@ def search_by_title(title):
 
 
 def search_by_date(date):
-    pass
+    try:
+
+        date_formated = (
+            datetime.strptime(date, "%Y-%m-%d")
+            .strftime("%d-%m-%Y")
+            .replace("-", "/")
+        )
+
+        news = search_news({"timestamp": date_formated})
+
+        list_news = []
+
+        for post in news:
+            list_news.append((post["title"], post["url"]))
+
+        return list_news
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 def search_by_category(category):
